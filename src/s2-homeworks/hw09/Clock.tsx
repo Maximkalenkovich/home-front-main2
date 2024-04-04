071279a062c1a62d2e04136f8d99bf1a1ac81f8e
Maximkalenkovich <peterson.maxim@mail.ru>
2024-04-04 10:18:32 +0300

    add 9 homework

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -21,7 +21,10 @@ stop()
 
     const stop = () => {
         // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
-        clearInterval(timerId)
+        if (timerId !== undefined) {
+            clearInterval(timerId);
+            setTimerId(undefined);
+        }
     }
 
     const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
@@ -77,14 +80,14 @@ setShow(false)
             <div className={s.buttonsContainer}>
                 <SuperButton
                     id={'hw9-button-start'}
-                    disabled={false} // пишут студенты // задизэйблить если таймер запущен
+                    disabled={timerId!==undefined} // пишут студенты // задизэйблить если таймер запущен
                     onClick={start}
                 >
                     start
                 </SuperButton>
                 <SuperButton
                     id={'hw9-button-stop'}
-                    disabled={false} // пишут студенты // задизэйблить если таймер не запущен
+                    disabled={timerId==undefined} // пишут студенты // задизэйблить если таймер не запущен
                     onClick={stop}
                 >
                     stop
